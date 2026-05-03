099c42280c8679dad2844314fcf1bce984ca018c
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-05-03 17:01:52 +0530

    Structure declaration with pragma pack which avoids the padding

---
+++ StructureDeclarationPragma.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+//Declaration
+#pragma pack(1)
+struct Demo
+{
+    int i;   //4
+    char ch ;   //1
+    float f;   //4
+};             //12
+int main()
+{
+struct Demo dobj;
+printf("%d\n",sizeof(dobj)); //12
+
+    return 0;
+}
