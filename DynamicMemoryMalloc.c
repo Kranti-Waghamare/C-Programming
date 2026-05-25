4bfcdaedccfa1f33fc894efb639f269f88c69c1b
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-05-25 10:57:29 +0530

    Dynamic  memory allocation in c using the malloc

---
+++ DynamicMemoryMalloc.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int size = 0;
+    float *Marks = NULL;
+    int i = 0;            //Loop Counter
+    
+    printf("Enter number of elements\n");
+    scanf("%d",&size);
+
+    //Dynamic Memory Allocation
+    Marks = (float*)malloc(size * sizeof(float));
+
+    printf("Enter your marks : \n");
+    
+    //Iteration
+    //   1      2      3
+    for(i = 0; i < size; i++)
+    {
+        scanf("%f",&Marks[i]);    //4
+    }
+    
+    printf("Entered marks are : \n");
+
+    //    1     2      3
+    for(i = 0; i < size; i++)
+    {
+        printf("%f\n",Marks[i]);    //4
+    }
+
+    free(Marks);
+    
+    return 0;
+}
